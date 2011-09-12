00714f1fc1e19ed5f58945ea8946e68fe70c452b
Gerred Dillon <gerred@gerred.com>
2011-09-12 15:18:04 -0600

    Updating to show developers and associated apps

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Idevdthis::Application.routes.draw do
   match "/auth/:provider/callback" => "sessions#create"
   match "/auth/logout" => "sessions#destroy", as: "logout"
   match "/apps/search" => "apps#search"
+  match "/:app/associate" => "apps#associate"
   match "/:app" => "apps#show"
   # The priority is based upon order of creation:
   # first created -> highest priority.
