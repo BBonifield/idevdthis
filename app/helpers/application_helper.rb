0118b260a7fd1d002b3b15d006ae321be513d289
Bob Bonifield <bobbonifield@gmail.com>
2011-09-13 11:19:31 -0600

    Making it so that the header image links to the root domain of each of our possible domains

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,14 @@ module ApplicationHelper
   def active_app_count
     App.linked_apps.count
   end
+
+  def path_to_root
+    if request.domain.match(/idevdthis\.com/)
+      "idevdthis.com"
+    elsif request.domain.match(/localhost/)
+      "localhost:#{request.port}"
+    elsif request.domain.match(/lvh.me/)
+      "lvh.me:#{request.port}"
+    end
+  end
 end
