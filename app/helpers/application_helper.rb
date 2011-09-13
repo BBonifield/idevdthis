1375cff519bf59754d48a00d5cdcbeed914a7ade
Bob Bonifield <bobbonifield@gmail.com>
2011-09-13 11:23:14 -0600

    Whoops, missing http

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,11 +9,11 @@ module ApplicationHelper
 
   def path_to_root
     if request.domain.match(/idevdthis\.com/)
-      "idevdthis.com"
+      "http://idevdthis.com"
     elsif request.domain.match(/localhost/)
-      "localhost:#{request.port}"
+      "http://localhost:#{request.port}"
     elsif request.domain.match(/lvh.me/)
-      "lvh.me:#{request.port}"
+      "http://lvh.me:#{request.port}"
     end
   end
 end
